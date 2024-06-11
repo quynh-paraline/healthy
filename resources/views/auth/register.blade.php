f5d005d4cb0cbc03a37aa5b5c42208f03aa8ce5d
anhnd-paraline <anhnd.paraline@gmail.com>
2024-06-11 14:45:23 +0700

    Add file login admin

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container" style="width: 650px;margin-top: 40px">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header"
+                         style="text-align: center;font-size: 30px;background-color: #4eb256">{{ __('Register') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+
+                            <div class="row mb-3">
+                                <div class="col-md-6" style="width: 350px;margin-left: 25px">
+                                    <input id="name" type="text" placeholder="Full Name"
+                                           class="form-control @error('name') is-invalid @enderror" name="name"
+                                           value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <div class="col-md-6" style="width: 350px;margin-left: 25px">
+                                    <input id="email" type="email" placeholder="Email Address"
+                                           class="form-control @error('email') is-invalid @enderror" name="email"
+                                           value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <div class="col-md-6" style="width: 350px;margin-left: 25px">
+                                    <input id="password" type="password" placeholder="Password"
+                                           class="form-control @error('password') is-invalid @enderror" name="password"
+                                           required autocomplete="new-password">
+
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <div class="col-md-6" style="width: 350px;margin-left: 25px">
+                                    <input id="password-confirm" type="password" placeholder="Confirm Password"
+                                           class="form-control" name="password_confirmation" required
+                                           autocomplete="new-password">
+                                </div>
+                            </div>
+
+                            <div class="row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary"
+                                            style="width: 200px;margin-left: -45px;background-color: #4eb256;border: 0 solid ">
+                                        {{ __('Register') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,78 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//AdminController
+Route::prefix('/admin')->as('admin.')->group(function () {
+    Route::middleware(["auth", "admin"])->group(function () {
+        Route::get('/', [AdminController::class, "dashboard"])->name('dashboard');
+        Route::get('/products/search', [AdminController::class, "search"])->name('search');
+
+        Route::prefix('/products')->as('products.')->group(function () {
+            Route::get('/index', [ProductController::class, "index"])->name('index');
+            Route::get('/create', [ProductController::class, "create"])->name('create');
+            Route::get("/edit/{product}", [ProductController::class, "edit"])->name('edit');
+            Route::post('/update/{product}', [ProductController::class, "update"])->name('update');
+            Route::post('/store', [ProductController::class, "store"])->name('store');
+            Route::get('/delete/{product}', [ProductController::class, "delete"])->name('delete');
+        });
+
+        Route::prefix('/categories')->as('categories.')->group(function () {
+            Route::get('/index', [CategoryController::class, "index"])->name('index');
+            Route::get('/create', [CategoryController::class, "create"])->name('create');
+            Route::get('/edit/{category}', [CategoryController::class, "edit"])->name('edit');
+            Route::post('/update/{category}', [CategoryController::class, "update"])->name('update');
+            Route::post('/store', [CategoryController::class, "store"])->name('store');
+            //edit post
+            Route::get('/delete/{category}', [CategoryController::class, "delete"])->name('delete');
+        });
+
+        Route::prefix('/orders')->as('orders.')->group(function () {
+            Route::get('/index', [OrderController::class, "index"])->name('index');
+            Route::get('/detail/{order}', [OrderController::class, "detail"])->name('detail');
+            Route::get('/filter', [OrderController::class, "filter"])->name('filter');
+        });
+
+    });
 });
+
+Route::prefix('/carts')->as('carts.')->group(function () {
+    Route::get('/index', [CartController::class, 'index'])->name('index');
+    Route::get('/add/{product}', [CartController::class, 'add'])->name('add');
+    Route::get('/reduce/{product}', [CartController::class, 'reduce'])->name('reduce');
+    Route::get('/remove/{product}', [CartController::class, 'remove'])->name('remove');
+    Route::get('/checkout', [CartController::class, "checkout"])->name('checkout');
+});
+
+//Auth::routes();
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
+
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+//PageController
+Route::get('/', [PageController::class, "welcome"])->name("welcome");
+Route::get('/contact', [PageController::class, "contact"])->name("contact");
+Route::get('/shop', [PageController::class, "shop"])->name("shop");
+Route::get('/products/{category}', [PageController::class, "productCategory"])->name('productsCategory');
+Route::get('/search', [PageController::class, "search"])->name('search');
+Route::get('/thankyou/{order}', [PageController::class, "thankyou"])->name('thankyou');
+
+
+Route::get('/cancelTransaction/{order}', [OrderController::class, "cancelTransaction"])->name('cancelTransaction');
+Route::get('/successTransaction/{order}', [OrderController::class, "successTransaction"])->name('successTransaction');
+Route::post('/placeOrder', [OrderController::class, "placeOrder"])->name('placeOrder');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
