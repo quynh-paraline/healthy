d3d790e52f62d7602e632043bd6f88fda4b7d6fc
anhnd-paraline <anhnd.paraline@gmail.com>
2024-06-13 09:06:07 +0700

    Edit and update directory admin

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\Auth\LoginController;
+use App\Http\Controllers\Admin\Auth\RegisterController;
+use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 //AdminController
@@ -45,34 +45,41 @@ Route::prefix('/admin')->as('admin.')->group(function () {
     });
 });
 
-Route::prefix('/carts')->as('carts.')->group(function () {
-    Route::get('/index', [CartController::class, 'index'])->name('index');
-    Route::get('/add/{product}', [CartController::class, 'add'])->name('add');
-    Route::get('/reduce/{product}', [CartController::class, 'reduce'])->name('reduce');
-    Route::get('/remove/{product}', [CartController::class, 'remove'])->name('remove');
-    Route::get('/checkout', [CartController::class, "checkout"])->name('checkout');
-});
+Route::prefix('/web')->as('web.')->group(function () {
 
-//Auth::routes();
-Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('register', [RegisterController::class, 'register']);
-
-Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('login', [LoginController::class, 'login']);
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+    Route::prefix('/carts')->as('carts.')->group(function () {
+        Route::get('/index', [CartController::class, 'index'])->name('index');
+        Route::get('/add/{product}', [CartController::class, 'add'])->name('add');
+        Route::get('/reduce/{product}', [CartController::class, 'reduce'])->name('reduce');
+        Route::get('/remove/{product}', [CartController::class, 'remove'])->name('remove');
+        Route::get('/checkout', [CartController::class, "checkout"])->name('checkout');
+    });
 
 //PageController
-Route::get('/', [PageController::class, "welcome"])->name("welcome");
-Route::get('/contact', [PageController::class, "contact"])->name("contact");
-Route::get('/shop', [PageController::class, "shop"])->name("shop");
-Route::get('/products/{category}', [PageController::class, "productCategory"])->name('productsCategory');
-Route::get('/search', [PageController::class, "search"])->name('search');
-Route::get('/thankyou/{order}', [PageController::class, "thankyou"])->name('thankyou');
+    Route::get('/', [PageController::class, "welcome"])->name("welcome");
+    Route::get('/contact', [PageController::class, "contact"])->name("contact");
+    Route::get('/shop', [PageController::class, "shop"])->name("shop");
+    Route::get('/products/{category}', [PageController::class, "productCategory"])->name('productsCategory');
+    Route::get('/thankyou/{order}', [PageController::class, "thankyou"])->name('thankyou');
 
+//Auth views
+    Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 
-Route::get('/cancelTransaction/{order}', [OrderController::class, "cancelTransaction"])->name('cancelTransaction');
-Route::get('/successTransaction/{order}', [OrderController::class, "successTransaction"])->name('successTransaction');
-Route::post('/placeOrder', [OrderController::class, "placeOrder"])->name('placeOrder');
+    Route::get('/cancelTransaction/{order}', [OrderController::class, "cancelTransaction"])->name('cancelTransaction');
+    Route::get('/successTransaction/{order}', [OrderController::class, "successTransaction"])->name('successTransaction');
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
 
+Route::prefix('/api')->as('api.')->group(function () {
+    Route::get('/search', [PageController::class, "search"])->name('search');
+
+    //Auth
+    Route::post('/register', [RegisterController::class, 'register']);
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    //PlaceOrder
+    Route::post('/placeOrder', [OrderController::class, "placeOrder"])->name('placeOrder');
+});
